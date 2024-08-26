1d93d7164e41cb5091d6e39a396fdfb329414654
Martín <msimonetti@gmail.com>
2024-08-26 00:26:02 -0300

    Se acomodó la estructura del proyecto. Se acomodó todo el router del producto. Se mejoró el manejo de los errores. Todo está testeado y funcionando.

---
+++ api/products/products.router.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const router = express.Router()
+const ProductManager = require('../../src/productManager')
+
+const productManager = new ProductManager("")
+
+router.get("/products", async (req, res) => { 
+    let consultas = req.query
+    const respuesta = await productManager.getProducts()
+    if(respuesta.status === "success"){
+        if (Object.keys(consultas).length !== 0) {            
+            let elementosMostrar =  respuesta.payload.slice(0, consultas.limit)
+            res.status(200).json(elementosMostrar)
+        } else {
+            res.status(200).json(respuesta.payload)
+        }
+    } else {
+        res.status(404).json(respuesta.payload)
+    }    
+})
+
+router.get("/products", async (req,res)=>{
+    let consultas = req.query
+    console.log(consultas)
+    //res.send(consultas)
+})
+
+router.get("/products/:pid", async (req, res) => {
+    const productoId = parseInt(req.params.pid)
+
+    const respuesta = await productManager.getProductById(productoId)
+    console.log(respuesta.status)
+    if(respuesta.status === "success"){
+        res.status(200).json(respuesta.payload)
+    } else {
+        res.status(200).json(respuesta.error)
+    }
+})
+
+router.post("/products", async (req, res) => {
+    const nuevoProducto = {
+        id: productManager.products.length + 1,
+        ...req.body
+    }    
+    const respuesta = await productManager.addProduct(nuevoProducto)
+    if(respuesta.status === "success"){
+        res.status(201).json(respuesta.payload)
+    } else {
+        res.status(404).json(respuesta.error)
+    }    
+})
+
+router.put("/products/:pid", async (req, res) => {
+    const productoId = parseInt(req.params.pid)
+
+    const respuesta = await productManager.editProduct(productoId, req.body)
+    
+    if(respuesta.status === "success"){
+        res.status(200).json(respuesta.payload)
+    } else {
+        res.status(404).json(respuesta.error)
+    }    
+})
+
+router.delete("/products/:pid", async (req, res) => {
+    const productoId = parseInt(req.params.pid)
+    
+    const respuesta = await productManager.deleteProduct(productoId)
+    
+    if(respuesta.status === "success"){
+        res.status(200).json(respuesta.payload)
+    } else {
+        res.status(404).json(respuesta.error)
+    }
+})
+
+module.exports = router
